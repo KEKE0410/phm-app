8b52ac4c0b978889eb58bbc94309ea85864f7cc0
KEKE0410 <keke0410@mail.fujios.net>
2025-02-20 15:48:49 +0900

    update

--- app/index.js
+++ app/index.js
@@ -1,10 +1,40 @@
-const { View, Text } = require("react-native");
-
+import { RNSerialport } from "@fugood/react-native-usb-serialport";
+import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
 const Page = () => {
+  const [deviceList, setDeviceList] = useState([]);
+
+  useEffect(() => {
+    const fetchDevices = async () => {
+      try {
+        const response = await RNSerialport.getDeviceList();
+        if (!response.status) {
+          console.log(
+            "Error from getDeviceList()",
+            response.errorCode,
+            response.errorMessage
+          );
+          return;
+        }
+        setDeviceList(response.devices);
+      } catch (error) {
+        console.error("Error fetching device list:", error);
+      }
+    };
+
+    fetchDevices();
+  }, []);
 
   return (
     <View>
       <Text>ホーム</Text>
+      {deviceList.length > 0 ? (
+        deviceList.map((device, index) => (
+          <Text key={index}>{device.deviceName}</Text>
+        ))
+      ) : (
+        <Text>デバイスが見つかりません</Text>
+      )}
     </View>
   );
 };
